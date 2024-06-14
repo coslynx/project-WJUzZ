da2d7a50026f3709685a4b83f13d6a722a2ccc59
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-14 12:28:39 +0530

    Added: `social-networking-platform/backend/controllers/EventPlanningController.js`

---
+++ social-networking-platform/backend/controllers/EventPlanningController.js
@@ -0,0 +1,29 @@
+const EventPlanningController = require('../models/Event');
+
+const createEvent = (req, res) => {
+  // Logic to create a new event
+};
+
+const getEvent = (req, res) => {
+  // Logic to get a specific event by ID
+};
+
+const getAllEvents = (req, res) => {
+  // Logic to get all events
+};
+
+const updateEvent = (req, res) => {
+  // Logic to update an existing event
+};
+
+const deleteEvent = (req, res) => {
+  // Logic to delete an event
+};
+
+module.exports = {
+  createEvent,
+  getEvent,
+  getAllEvents,
+  updateEvent,
+  deleteEvent,
+};
